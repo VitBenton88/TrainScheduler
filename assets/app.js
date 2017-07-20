dfe9f37b96726c9541880fbeb2496f570e5b72da
Vit Benton <Vitek@Vits-MacBook-Air.local>
2017-07-19 20:01:28 -0400

    basic functionality finished, used child_add listener to store and publish new trains added

--- assets/app.js
+++ assets/app.js
@@ -10,7 +10,10 @@ $(document).ready(function() {
 		  };
 		  firebase.initializeApp(config);
 
+		  var database = firebase.database();
+
 	//FIREBASE END ----------------------
+
 
 	
 	$('.submit').click(function(){
@@ -19,27 +22,45 @@ $(document).ready(function() {
 		var newDestination = $('#Destination').val().trim();
 		var newFirstTrainTime = $('#FirstTrainTime').val().trim();
 		var newFreq = $('#Freq').val().trim();
-		var currentTime = $.now();
-		var nextArrival = currentTime + newFreq;
-		var minutesAway = nextArrival - currentTime;
+		// var currentTime = $.now();
+		// var nextArrival = currentTime + newFreq;
+		// var minutesAway = nextArrival - currentTime;
 
-		//if any input in form is blank, alert and stop code:
-		if (newTrainName === ''||newDestination === ''||newFirstTrainTime === ''||newFreq === ''){
-			$('.alert-danger').css('display','initial')
-			return;
-		}
+		// if any input in form is blank, alert and stop code:
+		// if (newTrainName === ''||newDestination === ''||newFirstTrainTime === ''||newFreq === ''){
+		// 	$('.alert-danger').css('display','initial')
+		// 	return;
+		// }
+		// else {
+		// 	$('.alert-danger').css('display','none')
+		// };
 
-		else {
-			$('.alert-danger').css('display','none')
-		};
+		// store new train values to friebase:
 
-		//create new table row from user input:
-		$('table').append('<tr><td>' + newTrainName + '</td><td>' + newDestination + '</td><td>' + newFreq + '</td><td>' + newFirstTrainTime + '</td><td>' + minutesAway + '</td></tr>')
+		database.ref().push({
+
+			Name: newTrainName,
+			Destination: newDestination,
+			Freq: newFreq,
+		});
 
 		//clear form after submission:
 		for (var i = 0; i < $('form').length; i++) {
 			$('form')[i].reset();
 		};
+	});
+
+	//$('.submit').click END -----------------------------------------------
+
+	//publish two train objects, this is the default view:
+	// database.ref().on("value", function(snapshot) {
+	// 	$('tbody').append('<tr><td>' + snapshot.val().Train1.Name + '</td><td>' + snapshot.val().Train1.Destination + '</td><td>' + snapshot.val().Train1.Freq + '</td><td>' + snapshot.val().Train1.NextArr + '</td></tr>')
+	// 	$('tbody').append('<tr><td>' + snapshot.val().Train2.Name + '</td><td>' + snapshot.val().Train2.Destination + '</td><td>' + snapshot.val().Train2.Freq + '</td><td>' + snapshot.val().Train2.NextArr + '</td></tr>')  	
+	// });
+
+	//listen for additions made and publish accordingly:
+	database.ref().on("child_added", function(snapshot) {
+		$('tbody').append('<tr><td>' + snapshot.val().Name + '</td><td>' + snapshot.val().Destination + '</td><td>' + snapshot.val().Freq + '</td><td>' + snapshot.val().NextArr + '</td></tr>')
 	});
 
 //----------------------------------------------------------------END OF SCRIPT	
