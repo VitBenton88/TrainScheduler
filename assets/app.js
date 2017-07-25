0d01d45262477436e8550dc0999e2698e4bb6240
Vit Benton <vitbenton@Macintosh.local>
2017-07-24 21:16:40 -0400

    added update by the minute feature

--- assets/app.js
+++ assets/app.js
@@ -403,6 +403,78 @@ $(document).ready(function() {
 			});
 		});
 
+	function emptyAndUpdate (){
+		emptyTableBody();
+		database.ref().orderByChild("dateAdded").on("child_added", function(snapshot){
+
+			var FBname = snapshot.val().Name;//capture train name from firebase
+			var FBdestination = snapshot.val().Destination;//capture destination from firebase
+			var FBfreq = snapshot.val().Freq;//capture frequency from firebase
+			var FBarrivalTime = snapshot.val().firstArrival;//capture arrival time from firebase
+			var FBkeyValue = snapshot.key;//store key name to store in data attr, which is used for deleting
+
+   			var militaryFormat = "HH:mm";//set format for military time display
+   			var normalFormat = "hh:mm A";//set format for normal time display
+    		var militaryArrivalTime = moment(FBarrivalTime, militaryFormat);//format arrival time from firebase to display as military time
+    		var normalArrivalTime = moment(FBarrivalTime, normalFormat);//format arrival time from firebase to display as normal time
+    		var nextArrival;
+			var minAway;
+
+			function negativeMinAwayFix (){//if minutes away comes out to negative, reset time to original start time
+				if (minAway < -1){
+					minAway = minAway + 1440;//add total minutes in day (1440) to estimate because this will be a negative number otherwise
+				};
+			};
+
+			function alertArrivingNow(){//if minutes away is zero, change digits to string alerting of immenent arrival
+				if (minAway === 0){
+					minAway = '<p class="arrivingAlert">ARRIVING NOW</p>';
+				};
+			};
+
+			function publishData(){//post to HTML:
+				$('tbody').append('<tr><td class="name" data-key =' + FBkeyValue + '>'
+				 + FBname
+				 + '</td><td>' 
+				 + FBdestination
+				 + '</td><td>' 
+				 + FBfreq
+				 + '</td><td>'
+				 + nextArrival
+				 + '</td><td>'
+				 + minAway
+				 + '</td><td>'
+				 + '<span class="glyphicon glyphicon-remove" aria-hidden="true" data-key =' + FBkeyValue + '></span>'
+				 + '</td><td>'
+				 + '<button type="submit" class="btn btn-success btn-xs update" data-key =' + FBkeyValue + '>Update'
+				 + '</td></tr>')
+			};
+
+			function calculateTimes (Frequency, FirstArrivalTime){
+
+				for (var i = 0; i < 1440; i++) {//loop through minutes in day to cover all possibilities
+					if (moment().isSameOrBefore(FirstArrivalTime)){//if the current time is less than or equal to the provided 'first arrival time'...
+						minAway = moment(FirstArrivalTime).diff(moment(), "minutes",);//calculate difference between now and arrival time in minutes
+						nextArrival = moment(FirstArrivalTime, normalFormat).format(normalFormat);//the next arrival time will be the provided 'first arrival time' and will display in normal format
+					}
+
+					else {//if the current time is NOT less than or equal to the provided 'first arrival time'...
+						FirstArrivalTime = moment(FirstArrivalTime).add(Frequency, 'm');//add the provided frequency to the provided 'first arrival time'
+						minAway = moment(FirstArrivalTime).diff(moment(), "minutes",);//calculate difference between now and arrival time in minutes
+						nextArrival = moment(normalArrivalTime, normalFormat).format(normalFormat);//the next arrival time will be the original 'first arrival time' in firebase because it is technically the next day
+					};
+				};
+
+				negativeMinAwayFix();//check for negative minAway
+				alertArrivingNow();//alert if train is arriving right now
+			};
+			
+			calculateTimes(FBfreq, militaryArrivalTime);
+			publishData();//listen for additions made and publish accordingly, by timestamp:
+    	});
+	};
+
+	setInterval(emptyAndUpdate, 60000);
 
 //----------------------------------------------------------------END OF SCRIPT	
 });
